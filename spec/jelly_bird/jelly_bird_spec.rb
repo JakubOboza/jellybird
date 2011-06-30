c12427458480203c8da6769a6c811d1892b4baa7
JakubOboza <jakub.oboza@gmail.com>
2011-06-30 10:34:07 +0100

    added tests for gem

--- spec/jelly_bird/jelly_bird_spec.rb
+++ spec/jelly_bird/jelly_bird_spec.rb
@@ -59,6 +59,15 @@ describe JellyBird do
 
   describe "#gen" do
 
+    before(:all) do
+
+      Dummy.define {{
+        :name => /\w{3,8}/.gen,
+        :number => /\d\d\d\d\d\d\d/.gen
+      }}
+
+    end
+
     it "should be able to generate object" do
       dummy = Dummy.gen( :name => "test" )
       dummy.should be_kind_of(Dummy)
@@ -70,6 +79,26 @@ describe JellyBird do
         Dummy.gen[:name].should_not eql(Dummy.gen[:name])
       end
     end
+
+    it "should create object with other objects inside" do
+      dummy = Dummy.gen({:bomb => {:size => 15, :purpose => "anti-personal", :type => {:name => "claymore", :category => ["land", "mine"] }}})
+      dummy[:name].should_not be_nil
+      dummy[:bomb][:type][:category][1].should eql("mine")
+    end
+
+   it "should generate object inside of other object" do
+
+      # bug, currently for hashes you can input a gen inside of a define ;/
+      dummy = Dummy.gen
+
+      Rummy.define {{
+          :baby_rummy_is_a => dummy
+      }}
+
+      mummy = Rummy.gen
+      mummy[:baby_rummy_is_a][:name].should_not be_nil
+
+   end
 
   end
 
